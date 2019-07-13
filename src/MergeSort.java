62e1e150ec0105b989ed08df00b6f8ffbd3670c5
Atri Tripathi <tripathi.atri@gmail.com>
2019-07-13 22:46:18 +0530

    Add Merge sort implementation

---
+++ src/MergeSort.java
@@ -0,0 +1,48 @@
+public class MergeSort {
+    private void sort(int[] arr, int lb, int ub) {
+        if (lb < ub) {
+            int mid = (lb + ub)/2;
+            sort(arr,lb,mid);
+            sort(arr,mid+1,ub);
+
+            merge(arr,lb,mid,ub);
+        }
+    }
+
+    private void merge(int[] arr, int lb, int mid, int ub) {
+        int i=lb, j=mid+1, k=lb;
+        int[] tempArr = new int[arr.length];
+
+        while (i <= mid && j <= ub) {
+            if (arr[i] <= arr[j])
+                tempArr[k++] = arr[i++];
+            else
+                tempArr[k++] = arr[j++];
+        }
+
+        while (i < mid)
+            tempArr[k++] = arr[i++];
+        
+        while (j < ub)
+            tempArr[k++] = arr[j++];
+
+        for (int l = lb; l < ub; l++) {
+            arr[l] = tempArr[l];
+        }
+    }
+     private void printArray(int[] arr) {
+         for (int val: arr) {
+             System.out.print(val + " ");
+         }
+         System.out.println();
+     }
+
+    public static void main(String[] args) {
+//        int data[] = {7, 9, 5, 2, -5, -1, -2, 0, 4, 3};
+
+        int data[] = {2, 13, 5, 11, 7};
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(data,0,data.length-1);
+        mergeSort.printArray(data);
+    }
+}
